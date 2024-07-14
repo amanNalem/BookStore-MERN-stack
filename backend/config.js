f42a53b1dc92012c11f2ee2666b0c6e513e470d4
amanNalem <aman.nalem@gmail.com>
2024-07-14 00:35:22 -0700

    New Changes

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+'mongodb+srv://amannalem:sairam09@bookstore-mern-stack.brpfwfz.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore-MERN-stack'
